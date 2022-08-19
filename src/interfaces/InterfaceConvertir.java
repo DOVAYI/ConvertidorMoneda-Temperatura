94d6de9fccceaf0c36a12cd105f6f0c30a88a695
unknown <exadom21@gmail.com>
2022-08-19 06:38:31 -0500

    commit inicial

---
+++ src/interfaces/InterfaceConvertir.java
@@ -0,0 +1,12 @@
+
+package interfaces;
+
+/**
+ *
+ * @author Luis Alfredo Romero
+ */
+public interface InterfaceConvertir {
+    
+    public double conversion(String de,String para,double cantidad);
+    
+}
